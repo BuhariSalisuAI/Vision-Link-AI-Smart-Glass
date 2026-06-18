b70856f29acf360a6fb32af705cb3be0d3c9d133
BuhariSalisuAI <salisubuhari1025@gmail.com>
2026-06-18 07:24:48 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from speech_recognition import listen
+from speech_recognition import listen_for_command
 from text_to_speech import speak_hausa
 from object_detection import detect_objects
 from ocr_reader import read_text
@@ -8,7 +8,7 @@ def main():
     speak_hausa("Barka da rana! Vision-Link AI tana shirye.")
     
     while True:
-        command = listen()
+        command = listen_for_command()
         
         if "abubuwa" in command:
             objects = detect_objects()
